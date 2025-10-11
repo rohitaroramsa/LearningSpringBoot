0f1decfdd989616875b5d1864b1f431eb0c076e9
Rohit Arora <rohit.arora.msa@gmail.com>
2025-10-11 15:58:36 +0100

    wrong import of annotation

--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -2,9 +2,10 @@ package com.example.demo.service;
 
 import com.example.demo.model.Product;
 import com.example.demo.repository.ProductRepository;
-import jakarta.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
