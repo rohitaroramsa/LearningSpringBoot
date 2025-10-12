5f65f7cccc2b248b745e07bef12569de4afa7e5a
Rohit Arora <rohit.arora.msa@gmail.com>
2025-10-12 17:45:04 +0100

    added logic to verify deletion
    Integer to handle null, int is primitive

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -4,12 +4,8 @@ import com.example.demo.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    void deleteByProductNameIgnoreCase(String productName);
+    Integer deleteByProductNameIgnoreCase(String productName);
 }
 
