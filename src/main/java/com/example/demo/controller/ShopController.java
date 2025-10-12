94cdd098750c181f8a93ae332bd15d6e4f0cb4c5
Rohit Arora <rohit.arora.msa@gmail.com>
2025-10-12 20:59:10 +0100

    refactored improved standards

--- src/main/java/com/example/demo/controller/ShopController.java
+++ src/main/java/com/example/demo/controller/ShopController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import com.example.demo.model.Product;
 import com.example.demo.service.ProductService;
 import com.example.demo.service.ShopService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,52 +11,57 @@ import java.util.List;
 
 
 @RestController
+@RequestMapping("/shop")
 public class ShopController {
 
-    @Autowired
-    private ShopService shopStatus;
+    private final ShopService shopService;
 
-    @Autowired
-    private ProductService productService;
+    private final ProductService productService;
+
+    // dependency injection
+    public ShopController(ShopService shopService, ProductService productService) {
+        this.shopService = shopService;
+        this.productService = productService;
+    }
 
     @GetMapping(value = "/shop")
     public ResponseEntity<String> shopStatus(){
-        boolean shopOpen = Boolean.parseBoolean(this.shopStatus.shopOpen());
+        boolean shopOpen = Boolean.parseBoolean(this.shopService.shopOpen());
         if(shopOpen){
             return ResponseEntity.ok("shop is open");
         }
         else{
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("shop is not open");
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("shop is not open");
         }
     }
 
-    @GetMapping(value = "/products")
-    public ResponseEntity<String> productAvailable(@RequestParam String productName){
+    @GetMapping(value = "/products/{productName}")
+    public ResponseEntity<String> productAvailable(@PathVariable String productName){
         Boolean productAvailable = productService.productAvailable(productName);
         if(productAvailable) {
             return ResponseEntity.status(HttpStatus.OK).body(productName + " is available");
         }
         else {
-            return ResponseEntity.status(HttpStatus.OK).body(productName + " is not available");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(productName + " is not available");
         }
     }
 
-    @PostMapping(value="/add-product")
+    @PostMapping(value="/products")
     public ResponseEntity<String> addProduct(@RequestParam String productName, @RequestParam int productCost){
         Product product = new Product(productName, productCost);
         productService.addProduct(product);
-        return ResponseEntity.status(HttpStatus.OK).body("product added!");
+        return ResponseEntity.status(HttpStatus.CREATED).body("product added!");
     }
 
 
-    @DeleteMapping(value = "/delete-product")
-    public ResponseEntity<String> deleteProduct(@RequestParam String productName){
+    @DeleteMapping(value = "/products/{productName}")
+    public ResponseEntity<String> deleteProduct(@PathVariable String productName){
         int n = productService.removeProduct(productName);
         String body = n>0 ? "product deleted":"nothing to delete";
         return ResponseEntity.status(HttpStatus.OK).body(body);
     }
 
-    @GetMapping(value="/getAllProduct")
+    @GetMapping(value="/products/all")
     public ResponseEntity<List<Product>> getAllProduct(){
         return ResponseEntity.status(HttpStatus.OK).body(productService.getAllProduct());
     }
