d9ed83844c09ee52afdc05314f5c0cd2d9040de1
Rohit Arora <rohit.arora.msa@gmail.com>
2025-10-21 21:40:58 +0100

    minor refactoring

--- src/test/java/com/example/demo/controller/test/ShopControllerTest.java
+++ src/test/java/com/example/demo/controller/test/ShopControllerTest.java
@@ -1,24 +1,21 @@
-package com.example.demo.controller;
+package com.example.demo.controller.test;
 
 
+import com.example.demo.controller.ShopController;
 import com.example.demo.dto.ResponseDto;
 import com.example.demo.service.ProductService;
 import com.example.demo.service.ShopService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
  import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -37,7 +34,9 @@ class ShopControllerTest {
     @Test
     void getAllProduct() throws Exception {
         ResponseDto response = new ResponseDto("TV", new BigDecimal(1212), 121);
+
         when(productService.getAllProducts()).thenReturn(List.of(response));
+
          mockMvc.perform(get("/shop/products/all")).andExpect(status().isOk())
                  .andExpect(jsonPath("$[0].productName").value("TV"))
                  .andExpect(jsonPath("$[0].productCost").value(1212))
